68998b697edb11e18f72a7e8894c8b882720e3f0
codefutura <codefutura@localhost>
2019-05-10 21:08:20 -0400

    Correccion de los atributos y metodos

--- src/Negocio/Padres.java
+++ src/Negocio/Padres.java
@@ -10,27 +10,131 @@ package Negocio;
  * @author michael
  */
 public class Padres {
+   private int idPadre;
    private String nombre;
-   
-   private String Direccion;
-    
-    private String FechaDeNacimiento;
-    
-    private String Cedula;
-    
-    private String Email;
-    
-    private int titular_pago;
+   private String direccion;
+   private String fechaNacimiento;
+   private String cedula;
+   private String email;
+   private boolean titularPago;
 
+    public Padres(int idPadre, String nombre, String direccion, String fechaNacimiento, String cedula, String email, boolean titularPago) {
+        this.idPadre = idPadre;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.fechaNacimiento = fechaNacimiento;
+        this.cedula = cedula;
+        this.email = email;
+        this.titularPago = titularPago;
+    }
+   
+    public Padres(){
+
+    };
+
+
+    /**
+     * @return the idPadre
+     */
+    public int getIdPadre() {
+        return idPadre;
+    }
+
+    /**
+     * @param idPadre the idPadre to set
+     */
+    public void setIdPadre(int idPadre) {
+        this.idPadre = idPadre;
+    }
+
+    /**
+     * @return the nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * @param nombre the nombre to set
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the fechaNacimiento
+     */
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    /**
+     * @param fechaNacimiento the fechaNacimiento to set
+     */
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    /**
+     * @return the cedula
+     */
+    public String getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the titularPago
+     */
+    public boolean isTitularPago() {
+        return titularPago;
+    }
+
+    /**
+     * @param titularPago the titularPago to set
+     */
+    public void setTitularPago(boolean titularPago) {
+        this.titularPago = titularPago;
+    }
+   
+
+
     
-    
+
     
    
 }
